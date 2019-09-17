a99dcdb837b9de2bfc5c3d2dd2cff7731d3f17f6
Joshua Holtsclaw <Jildsmiles@yahoo.com>
2019-09-17 16:14:41 -0400

    Project start. movie cards showing.

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -9,15 +9,16 @@ const Movie = (props) => {
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
-       axios
+      axios
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
           setMovie(response.data);
+          
         })
         .catch(error => {
           console.error(error);
         });
-
+        
   },[]);
   
   // Uncomment this only when you have moved on to the stretch goals
@@ -29,7 +30,7 @@ const Movie = (props) => {
   if (!movie) {
     return <div>Loading movie information...</div>;
   }
-
+  console.log(movie);
   const { title, director, metascore, stars } = movie;
   return (
     <div className="save-wrapper">
